610fa7b261a18912278022e0597f22a168686ff6
clo-lpT <hama4342@naver.com>
2022-05-20 15:00:59 +0900

    2022.05.20 address search & display on map

--- src/components/SearchAddress.vue
+++ src/components/SearchAddress.vue
@@ -1,64 +1,72 @@
 <template>
   <div id="SearchAddress">
     <div class="main_class">
-      <div class="sub_class">
-        <input type="text" id="sample5_address" placeholder="주소를 입력해주세용">
+      <div class="find_juso_map">
         <input type="button" v-on:click="load_juso" value="주소 검색"><br>
-        <div id="map" style="width:300px;height:300px;margin-top:10px;display:none"></div>
+        <div id="map" style="width:300px;height:300px;margin-top:10px;"></div> 
+
+        <input type="text" id="sample5_address" placeholder="메모">
+        <input type="button" v-on:click="add_place" value="장소 추가"><br>
+      </div>
+      <div class="set_juso_list">
+        <h4>요기조아</h4>
+        <div></div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-//     mapOption = {
-//         center: new window.daum.maps.LatLng(37.537187, 127.005476), // 지도의 중심좌표
-//         level: 5 // 지도의 확대 레벨
-//     };
 
-// //지도를 미리 생성
-// var map = new window.daum.maps.Map(mapContainer, mapOption);
-// //주소-좌표 변환 객체를 생성
-// var geocoder = new window.daum.maps.services.Geocoder();
-// //마커를 미리 생성
-// var marker = new window.daum.maps.Marker({
-//     position: new window.daum.maps.LatLng(37.537187, 127.005476),
-//     map: map
-// });
 export default {
   name: 'SearchAddress',
+  data() {
+    return {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      geocoder: new window.kakao.maps.services.Geocoder(),
+      joah_add: 'test',
+      joah_list: {}
+    }
+  },
   props: {
+  },
+  mounted () {
+  var container = document.getElementById('map');
+      var options = {
+        center: this.center,
+        level: 3
+      };
+  new window.daum.maps.Map(container, options);
   },
   methods: {
     load_juso() {
-      console.log('불렸다')
       new window.daum.Postcode({
-            oncomplete: function(data) {
-                var addr = data.address; // 최종 주소 변수
-
-                // // 주소 정보를 해당 필드에 넣는다.
-                document.getElementById("sample5_address").value = addr;
-                // // 주소로 상세 정보를 검색
-                // geocoder.addressSearch(data.address, function(results, status) {
-                //     // 정상적으로 검색이 완료됐으면
-                //     if (status === window.daum.maps.services.Status.OK) {
-
-                //         var result = results[0]; //첫번째 결과의 값을 활용
-
-                //         // 해당 주소에 대한 좌표를 받아서
-                //         var coords = new window.daum.maps.LatLng(result.y, result.x);
-                //         // 지도를 보여준다.
-                //         mapContainer.style.display = "block";
-                //         map.relayout();
-                //         // 지도 중심을 변경한다.
-                //         map.setCenter(coords);
-                //         // 마커를 결과값으로 받은 위치로 옮긴다.
-                //         marker.setPosition(coords)
-                //     }
-                // });
+        oncomplete: function(data) {
+          var addr = data.address; // 최종 주소 변수
+          console.log(addr,'주소')
+          this.joah_add = addr;
+          console.log(this.joah_add ,'조아조아')
+          // // 주소로 상세 정보를 검색
+          new window.kakao.maps.services.Geocoder().addressSearch(data.address, function(results, status) {
+            // 정상적으로 검색이 완료됐으면
+            if (status === window.daum.maps.services.Status.OK) {
+              var result = results[0]; //첫번째 결과의 값을 활용
+              // 해당 주소에 대한 좌표를 받아서
+              var coords = new window.daum.maps.LatLng(result.y, result.x);
+              // 지도를 보여준다.
+              var container = document.getElementById('map');
+              var map = new window.daum.maps.Map(container, {center: coords,level: 3});
+              container.style.display = "block";
+              map.relayout();
+              map.setCenter(coords);
+              new window.daum.maps.Marker({position: coords, map: map}).setPosition(coords);
             }
-        }).open();
+          });
+        }
+      }).open();
+    },
+    add_place() {
+      console.log('장소추가',this.joah_add)
     }
   }
 }
@@ -69,7 +77,13 @@ export default {
   display: flex;
 }
 
-#SearchAddress .main_class .sub_class .juso_input {
+#SearchAddress .main_class {
+  display: flex;
+  width: 100%;
+  justify-content: space-evenly;
+}
+
+#SearchAddress .main_class .find_juso_map .juso_input {
   display: flex;
   border-bottom: 2px solid gray;
 }
